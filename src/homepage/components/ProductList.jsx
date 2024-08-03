fee1185032efc039c3d704d6cead60bc2a45af68
1hyung <ryu1hyung@naver.com>
2024-08-03 19:12:20 +0900

    :bug: Fix: fix data connect

--- src/homepage/components/ProductList.jsx
+++ src/homepage/components/ProductList.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import { Grid, Card, Image, Text } from '@mantine/core';
+import React, {useEffect} from 'react';
+import {Grid, Card, Image, Text} from '@mantine/core';
+import {getAllProducts} from "../../api/v1/product/product.js";
 
 const products = Array(6).fill(0).map((_, index) => ({
-  id: index,
-  title: 'Text',
-  price: '$0',
-  description: 'Body text.',
-  imageUrl: '/path/to/your/image.jpg',
+    id: index,
+    title: 'Text',
+    price: '$0',
+    description: 'Body text.',
+    imageUrl: '/path/to/your/image.jpg',
 }));
 
 const ProductList = () => {
-  return (
-    <Grid mt="md">
-      {products.map((product) => (
-        <Grid.Col span={4} key={product.id}>
-          <Card shadow="sm" padding="lg">
-            <Card.Section>
-              <Image src={product.imageUrl} height={160} alt="상품 이미지" />
-            </Card.Section>
-            <Text weight={500} mt="md">{product.title}</Text>
-            <Text size="sm" color="dimmed">{product.price}</Text>
-            <Text size="sm" mt="sm">{product.description}</Text>
-          </Card>
-        </Grid.Col>
-      ))}
-    </Grid>
-  );
+    // API를 호출하여 상품 목록을 가져오는 로직
+    const getProducts = async () => {
+        console.log("getProducts");
+        const data = await getAllProducts();
+        console.log("data",data);
+    }
+    useEffect(() => {
+        getProducts();
+    }, []);
+    return (
+        <Grid mt="md">
+            {products.map((product) => (
+                <Grid.Col span={4} key={product.id}>
+                    <Card shadow="sm" padding="lg">
+                        <Card.Section>
+                            <Image src={product.imageUrl} height={160} alt="상품 이미지"/>
+                        </Card.Section>
+                        <Text weight={500} mt="md">{product.title}</Text>
+                        <Text size="sm">{product.price}</Text>
+                        <Text size="sm" mt="sm">{product.description}</Text>
+                    </Card>
+                </Grid.Col>
+            ))}
+        </Grid>
+    );
 };
 
 export default ProductList;
