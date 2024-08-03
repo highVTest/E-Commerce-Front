2ca3e1294ec3847780780a9056f8999e54657248
1hyung <ryu1hyung@naver.com>
2024-08-03 20:57:43 +0900

    :bug: Fix: fix connect ProductImage

--- src/homepage/components/ProductList.jsx
+++ src/homepage/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Grid, Card, Image, Text} from '@mantine/core';
 import {getAllProducts} from "../../api/v1/product/product.js";
 
@@ -11,11 +11,13 @@ const products = Array(6).fill(0).map((_, index) => ({
 }));
 
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
     // API를 호출하여 상품 목록을 가져오는 로직
     const getProducts = async () => {
         console.log("getProducts");
         const data = await getAllProducts();
         console.log("data",data);
+        setProducts(data.data.content);
     }
     useEffect(() => {
         getProducts();
@@ -26,7 +28,7 @@ const ProductList = () => {
                 <Grid.Col span={4} key={product.id}>
                     <Card shadow="sm" padding="lg">
                         <Card.Section>
-                            <Image src={product.imageUrl} height={160} alt="상품 이미지"/>
+                            <Image src={product.productImage} height={160} alt="상품 이미지" fallbackSrc="https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-for-website-design-or-mobile-app-no-photo-available_87543-11093.jpg"/>
                         </Card.Section>
                         <Text weight={500} mt="md">{product.title}</Text>
                         <Text size="sm">{product.price}</Text>
