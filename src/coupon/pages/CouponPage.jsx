93b9f8c5a45c486eb39ea24cd24969855cfaf406
Ppajingae <wkmkyj991112@gmail.com>
2024-08-16 10:49:20 +0900

    fix: 충돌 해결

--- src/coupon/pages/CouponPage.jsx
+++ src/coupon/pages/CouponPage.jsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import "../css/CouponPage.css"
 import {Image} from "@mantine/core";
-import {deleteSellerCoupon, getSellerCouponList} from "../../api/v1/coupon/coupon.js";
+import { getSellerCouponList} from "../../api/v1/coupon/coupon.js";
 import UpdateCouponModal from "../components/UpdateCouponModal.jsx";
 import SellerNavComponent from "../../sellerbackoffice/component/SellerNavComponent.jsx";
 
@@ -31,16 +31,6 @@ const CouponPage=()=>{
         if(discountPolicy === 'DISCOUNT_RATE'){
             return '할인율 정책'
         }else return '가격 할인 정책'
-    }
-
-    const deleteCoupon = async (id) => {
-        try {
-            await deleteSellerCoupon(token, id)
-            alert("쿠폰 삭제가 완료 되었습니다")
-        }catch (error) {
-            alert(error.response.data.errorMessage);
-        }
-
     }
 
 
