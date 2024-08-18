febda85771331e16675002fd8e37df6f038a4409
HR <dev.muun07@gmail.com>
2024-08-18 17:21:54 +0900

    mycoupon

--- src/coupon/pages/MyCouponPage.jsx
+++ src/coupon/pages/MyCouponPage.jsx
@@ -39,83 +39,63 @@ const MyCouponPage = () => {
 
   return (
     <>
-      <Text
-        size="xl"
-        fw={900}
-        variant="gradient"
-        gradient={{ from: "black", to: "blue", deg: 100 }}
-        style={{ textAlign: "center", fontSize: 50 }}
-      >
-        내 쿠폰 목록
-      </Text>
-      <ul>
-        <Stack
-          // h={shop.items.length == 1 ? 250 : 370}
-          bg="var(--mantine-color-body)"
-          align="stretch"
-          justify="center"
-          gap="xs"
-          style={{ marginTop: "10px" }}
-        >
-          {coupons.map((coupon) => {
-            return (
-              // eslint-disable-next-line react/jsx-key
-              <Grid
-                style={{
-                  height: "280px",
-                  justifyContent: "center",
+      <h1>쿠폰 목록</h1>
+      {coupons.map((coupon) => {
+        return (
+          <div key={coupon.couponId}>
+            <Fieldset
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "15px",
+              }}
+            >
+              <Stack style={{ textAlign: "left", marginLeft: "10px" }}>
+                <h2 style={{ margin: "0" ,marginBottom:"10px"}}>{coupon.couponName}</h2>
+                {coupon.discountPolicy === "DISCOUNT_RATE" ? (
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <p
+                      style={{ margin: "0", fontWeight: "600", width: "90px" }}
+                    >
+                      할인율
+                    </p>
+                    <p style={{ margin: "0" }}>할인율 : {coupon.discount} %</p>
+                  </div>
+                ) : (
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <p
+                      style={{ margin: "0", fontWeight: "600", width: "90px" }}
+                    >
+                      할인 금액
+                    </p>
+                    <p style={{ margin: "0" }}>
+                      할인 금액 : {coupon.discount} 원
+                    </p>
+                  </div>
+                )}
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <p style={{ margin: "0", fontWeight: "600", width: "90px" }}>
+                    만료 시간
+                  </p>
+                  <p style={{ margin: "0" }}>
+                    {coupon.expiredAt.split("-")[0]} 년{" "}
+                    {coupon.expiredAt.split("-")[1]} 월{" "}
+                    {coupon.expiredAt.split("-")[2].slice(0, 2)} 일 까지
+                  </p>
+                </div>
+              </Stack>
+
+              <Button
+                onClick={() => {
+                  getDeleteBuyerCoupon(coupon.couponId);
                 }}
-                key={coupon.couponId}
               >
-                <Fieldset style={{ marginLeft: "20px", marginTop: "30px" }}>
-                  <Grid></Grid>
-                  <Group
-                    gap="xs"
-                    grow
-                    style={{ margin: "10px", width: "1080px", height: "200px" }}
-                  >
-                    <Grid.Col span={9}>
-                      <Stack align="flex-start" justify="center" gap="md">
-                        <Text fw={1000} size="xl">
-                          {coupon.couponName}{" "}
-                        </Text>
-                        {coupon.discountPolicy === "DISCOUNT_RATE" ? (
-                          <Text fw={800} size="lg">
-                            {coupon.discount} % 할인
-                          </Text>
-                        ) : (
-                          <Text fw={800} size="lg">
-                            {coupon.discount} 원 할인
-                          </Text>
-                        )}
-                        <Text fw={800} size="lg" mt="md">
-                          만료 시간 : {coupon.expiredAt.split("-")[0]} 년{" "}
-                          {coupon.expiredAt.split("-")[1]} 월{" "}
-                          {coupon.expiredAt.split("-")[2].slice(0, 2)} 일 까지
-                        </Text>
-                      </Stack>
-                    </Grid.Col>
-                    <Grid.Col span={2}>
-                      <Button
-                        color="gray"
-                        // fullWidth
-                        mt="md"
-                        radius="md"
-                        style={{ width: "100px", height: "50px" }}
-                        onClick={() => {
-                          getDeleteBuyerCoupon(coupon.couponId);
-                        }}
-                      >
-                        삭제 하기
-                      </Button>
-                    </Grid.Col>
-                  </Group>
-                </Fieldset>
-              </Grid>
-            );
-          })}
-        </Stack>
-      </ul>
+                삭제 하기
+              </Button>
+            </Fieldset>
+          </div>
+        );
+      })}
     </>
   );
 };
